9657200c2be561821bdddeecd47cc722959d15bc
Chase Lones <chaselones@gmail.com>
2024-09-23 13:43:14 -0700

    added express.json, dotenv.config and cors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use('dotenv').config()
+app.use(cors())
 
 const PORT = process.env.PORT || 3001
 
